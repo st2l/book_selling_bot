f3052681f650ad1b972c4f3da2118d52f4749829
st2l <dolka007st@gmail.com>
2025-02-02 22:39:30 +0300

    i despise this class

--- utils/identify_user.py
+++ utils/identify_user.py
@@ -3,12 +3,16 @@ from aiogram.types import Message, CallbackQuery
 
 
 async def identify_user(message: Message | CallbackQuery):
+    id_ = message.from_user.id
+    first_name = message.from_user.first_name if message.from_user.first_name else "no_name"
+    last_name = message.from_user.last_name if message.from_user.last_name else "no_name"
+
     user, is_new = await User.objects.aget_or_create(
         pk=message.from_user.id,
         defaults={
-            "username": message.from_user.id,
-            "first_name": message.from_user.first_name,
-            "last_name": message.from_user.last_name,
+            "username": id_,
+            "first_name": first_name,
+            "last_name": last_name,
         },
     )
 
